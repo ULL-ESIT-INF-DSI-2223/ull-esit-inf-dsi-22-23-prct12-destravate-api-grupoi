529f87fdf5d8443694a9b0ba5118779403c76026
Kevinss02 <alu0101426846@ull.edu.es>
2023-05-14 18:29:19 +0000

    Añadida dependencias Ajv

--- dist/App.js
+++ dist/App.js
@@ -15,8 +15,7 @@ app.use(bodyParser.json());
 /**
  * Para conectarse a la base de datos
  */
-//connect('mongodb://127.0.0.1:27017/actividadesDeportivas').then(() => {
-connect('mongodb+srv://2012ivhda:Z7ZhekVI@cluster0.qdchiek.mongodb.net/?retryWrites=true&w=majority').then(() => {
+connect('mongodb://127.0.0.1:27017/actividadesDeportivas').then(() => {
     console.log('Connected to the database');
 }).catch(() => {
     console.log('Something went wrong when conecting to the database');
